0f0a6efd8e59cf28970a298bfe3da1596abecd79
Mike <mike.wildcats@gmail.com>
2017-11-01 23:38:26 -0400

    Actually include TODO
    derp

--- Raffler/MainWindow.xaml.cs
+++ Raffler/MainWindow.xaml.cs
@@ -33,6 +33,14 @@ namespace Raffler
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.ShowDialog();
 
+            //TODO: Read JSON file in and deserialize it to a list of objects
+            //TODO: Cycle through list printing a line with the UUID and decrementing the count associated and incrementing the number of rows
+            //TODO: Once List contains no records, select a number between 1 and the number of rows
+            //TODO: Display the record indexed at the rand result
+            //TODO: Hookup functions to events
+            //TODO: Select winner for October
+            //TODO: Attempt file data validation
+
         }
 
     }

86990ac659cd6afd60cefcc6f00feba71c80bdb2
Mike <mike.wildcats@gmail.com>
2017-11-01 23:37:50 -0400

    TODO list
    Minor edits to designer xaml

--- Raffler/MainWindow.xaml.cs
+++ Raffler/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Raffler
 {
     /// <summary>
@@ -24,5 +27,13 @@ namespace Raffler
         {
             InitializeComponent();
         }
+
+        private void btnOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+
+        }
+
     }
 }
